9ed1875586369479745aaa4d47323b9c13fe0403
DmitriyKondratyuk <k-d-m@ukr.net>
2016-09-09 21:47:00 +0300

    first comm

---
+++ js/maine.js
@@ -0,0 +1,22 @@
+﻿
+$(function() {
+  $('button').click(function() {
+    var but = $(this);
+    var time = 30; //задаем время в течении которого кнопка будет не активна
+    var timeInterval = setInterval(function() {
+
+      if (time <= 0) {
+        clearInterval(timeInterval);
+      }
+      if (time == 0) {
+        $(but).children('span').html("");
+        $(but).prop("disabled", false);
+      } else {
+        $(but).children('span').html(time);
+        $(but).prop("disabled", true);
+      }
+      time = time - 1;
+    }, 1000);
+
+  });
+});
